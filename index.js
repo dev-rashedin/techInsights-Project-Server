a4f6c80c531f31f6953f100007119f9fb01b6b4f
RU063 <rashedinislam.06@gmail.com>
2024-09-10 22:49:02 +0600

    mongodb setup successfully

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://byteblog-da679.web.app',
-    'https://byteblog-da679.firebaseapp.com',
+    'https://tech-insights-d2159.web.app',
+    'https://tech-insights-d2159.firebaseapp.com',
   ],
   credentials: true,
   optionSuccessStatus: 200,
@@ -24,6 +24,35 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4qgkjzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
