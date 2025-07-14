21d5e6cc9cc1e814a64ef74ddb2f8b3e213e595d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-14 20:39:45 +0600

    my own express-error-toolkit installed and used

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import { globalErrorHandler, notFoundHandler, StatusCodes } from "express-error-toolkit";
 
 const app: Application = express();
 const cors = require("cors");
@@ -7,4 +8,24 @@ app.use(express.json());
 
 app.use(cors());
 
+
+// home route
+app.get('/', (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: 'Welcome to The-Tech-Insight server'
+  });
+})
+
+// not found hanlder
+app.use(notFoundHandler);
+
+// global error handler
+app.use(globalErrorHandler);
+
 export default app;
+
+
+
+
+
